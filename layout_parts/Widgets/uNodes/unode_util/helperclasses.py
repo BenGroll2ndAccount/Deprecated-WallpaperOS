1652d2af012e2716ccdb07a72afc5e78da638c49
BenGrollPMT <bengroll002@gmail.com>
2022-04-27 14:47:20 +0200

    Polygonal Point Detection implemented, flawed. Needs good triangle-point-detection-algorihm

--- layout_parts/Widgets/uNodes/unode_util/helperclasses.py
+++ layout_parts/Widgets/uNodes/unode_util/helperclasses.py
@@ -21,14 +21,11 @@ class uPoint():
     def isInArea(self, constraint):
         return self.x > constraint.pointA.x and self.x < constraint.pointB.x and self.y > constraint.pointA.y and self.y < constraint.pointB.y
         
-
-
-
-
 class uConstrain():
-    def __init__(self, pointA : uPoint, pointB : uPoint):
+    def __init__(self, pointA : uPoint, pointB : uPoint, polypoints = None):
         self.pointA = uPoint(x = min([pointA.x, pointB.x]), y = min([pointA.y, pointB.y]))
         self.pointB = uPoint(x = max([pointA.x, pointB.x]), y = max([pointA.y, pointB.y]))
+        self.polypoints = polypoints
 
     def out(self) -> str:
         return "(" + str(self.pointA.x) + "|" + str(self.pointA.y) + "),(" + str(self.pointB.x) + "|" + str(self.pointB.y) + ")"
@@ -42,27 +39,28 @@ class uConstrain():
 
     @property
     def copy(self):
-        return uConstrain(pointA=uPoint(x = self.pointA.x, y = self.pointA.y), pointB=uPoint(x = self.pointB.x, y = self.pointB.y ))
+        return uConstrain(pointA=uPoint(x = self.pointA.x, y = self.pointA.y), pointB=uPoint(x = self.pointB.x, y = self.pointB.y), polypoints=self.polypoints.copy)
 
     @property
     def center(self):
         return uPoint(x = self.pointA.x + ((self.pointB.x - self.pointA.x) / 2), y = self.pointA.y + ((self.pointB.y - self.pointA.y) / 2))
     
     def isSafe(self, other_const, name):
-        if self.pointA.x < other_const.pointA.x:
-            input()
-            raise uBUILDTIMEEXCEPTION("Constrain out of bounds. May fix overlap or put it on another level.", name)
-        if self.pointB.x > other_const.pointB.x:
-            input()
-            raise uBUILDTIMEEXCEPTION("Constrain out of bounds. May fix overlap or put it on another level.", name)
-        if self.pointA.y < other_const.pointA.y: 
-            input()
-            raise uBUILDTIMEEXCEPTION("Constrain out of bounds. May fix overlap or put it on another level.", name)
-        if self.pointB.y > other_const.pointB.y:
-            input()
-            raise uBUILDTIMEEXCEPTION("Constrain out of bounds. May fix overlap or put it on another level.", name)
-        else:
-            return True
+        if self.polypoints == None and other_const.polypoints == None:
+            if self.pointA.x < other_const.pointA.x:
+                input()
+                raise uBUILDTIMEEXCEPTION("Constrain out of bounds. May fix overlap or put it on another level.", name)
+            if self.pointB.x > other_const.pointB.x:
+                input()
+                raise uBUILDTIMEEXCEPTION("Constrain out of bounds. May fix overlap or put it on another level.", name)
+            if self.pointA.y < other_const.pointA.y: 
+                input()
+                raise uBUILDTIMEEXCEPTION("Constrain out of bounds. May fix overlap or put it on another level.", name)
+            if self.pointB.y > other_const.pointB.y:
+                input()
+                raise uBUILDTIMEEXCEPTION("Constrain out of bounds. May fix overlap or put it on another level.", name)
+            else:
+                return True
 
 class Task():
     def __init__(self, title : str, date : str = None, time : str = None, endtime : str = None, category : str = None, description : str = None, key : str = ""):
