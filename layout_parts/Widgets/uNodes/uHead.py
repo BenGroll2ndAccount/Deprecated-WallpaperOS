579c705f5667c6117f263c1a782065533d7253d7
IfClause <ifclause002@gmail.com>
2022-05-14 14:55:55 +0200

    Widget Content Constraints now adapt to opening and closing of ccenter.

--- layout_parts/Widgets/uNodes/uHead.py
+++ layout_parts/Widgets/uNodes/uHead.py
@@ -70,37 +70,76 @@ class uHEAD(uNODE):
     @tlog
     def constrainmod(self):
         self.constraint = uConstrain(pointA = self.anchor, pointB = uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height))
-        __HEADERSIZE__ : int = NotifyService.get("debug.widget-header_thickness_in_clusters")
+        __HEADERSIZE__ : float = NotifyService.get("debug.widget-header_thickness_in_clusters")
         __CLUSTERRESOLUTION__ : int = NotifyService.get("debug.display-cluster_resolution")
+        __CCENTERSIZE__ : float = NotifyService.get("debug.widget-controlcenter-thickness-in-clusters")
         new_constraint = None
-        if self.header == "t":
-            new_constraint = uConstrain(pointA=uPoint(x=self.anchor.x, y = __HEADERSIZE__ * __CLUSTERRESOLUTION__), pointB=uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height))
-        elif self.header == "l":
-            new_constraint = uConstrain(pointA=uPoint(x=self.anchor.x + __HEADERSIZE__ * __CLUSTERRESOLUTION__, y = self.anchor.y), pointB=uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height))
-        elif self.header == "r":
-            new_constraint = uConstrain(pointA = self.anchor, pointB = uPoint(x = self.anchor.x + self.width - (__HEADERSIZE__ * __CLUSTERRESOLUTION__), y = self.anchor.y + self.height))
-        elif self.header == "b":
-            new_constraint = uConstrain(pointA = self.anchor, pointB = uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height - (__HEADERSIZE__ * __CLUSTERRESOLUTION__)))
+        if self.controlcenter.status == "Closed":
+            self.controlcenter.constrainmod(uConstrain(pointA=uPoint(0,0), pointB = uPoint(0,0)))
+            if self.header == "t":
+                new_constraint = uConstrain(pointA=uPoint(x=self.anchor.x, y = __HEADERSIZE__ * __CLUSTERRESOLUTION__), pointB=uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height))
+                self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * 0.6, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * 0.6),pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * 0.2, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * 0.2)))
+            elif self.header == "l":
+                new_constraint = uConstrain(pointA=uPoint(x=self.anchor.x + __HEADERSIZE__ * __CLUSTERRESOLUTION__, y = self.anchor.y), pointB=uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height))
+                self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * 0.6, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * 0.6),pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * 0.2, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * 0.2)))
+
+            elif self.header == "r":
+                new_constraint = uConstrain(pointA = self.anchor, pointB = uPoint(x = self.anchor.x + self.width - (__HEADERSIZE__ * __CLUSTERRESOLUTION__), y = self.anchor.y + self.height))
+                self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * 0.6, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * 0.6 - __CLUSTERRESOLUTION__ * __HEADERSIZE__),pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * 0.2 - __CLUSTERRESOLUTION__ * __HEADERSIZE__, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * 0.2)))
+
+            elif self.header == "b":
+                new_constraint = uConstrain(pointA = self.anchor, pointB = uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height - (__HEADERSIZE__ * __CLUSTERRESOLUTION__)))
+                self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * 0.6 + __CLUSTERRESOLUTION__ * 0.5, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * 0.6 - __CLUSTERRESOLUTION__ * __HEADERSIZE__ + __CLUSTERRESOLUTION__ * 0.5),pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * 0.2 - __CLUSTERRESOLUTION__ * __HEADERSIZE__+ __CLUSTERRESOLUTION__ * 0.5, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * 0.2+ __CLUSTERRESOLUTION__ * 0.5)))
+            else:
+                return self.child.constrainmod(self.constraint.copy)
         else:
-            return self.child.constrainmod(self.constraint.copy)
+            if self.header == "t":
+                #Set Widget Constraint
+                new_constraint = uConstrain(pointA=uPoint(x=self.anchor.x, y = __HEADERSIZE__ * __CLUSTERRESOLUTION__), pointB=uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height - __CLUSTERRESOLUTION__ * __CCENTERSIZE__))
+                #Set CCenter Constraint
+                button_shrinking_factor = 0.3
+                self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor) + __CLUSTERRESOLUTION__ * __CCENTERSIZE__), pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__  + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor) + __CLUSTERRESOLUTION__ * __CCENTERSIZE__)))
+                self.controlcenter.constrainmod(uConstrain(pointA=uPoint(x = new_constraint.pointA.x, y = new_constraint.pointB.y + __CCENTERSIZE__ * __CLUSTERRESOLUTION__), pointB=uPoint(x = new_constraint.pointB.x, y = new_constraint.pointB.y)))
+            elif self.header == "l":
+                #Set Widget Constraint
+                new_constraint = uConstrain(pointA=uPoint(x=self.anchor.x + __HEADERSIZE__ * __CLUSTERRESOLUTION__, y = self.anchor.y), pointB=uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height - __CCENTERSIZE__ * __CLUSTERRESOLUTION__))
+                #Set CCenter Constraint
+                button_shrinking_factor = 0.3
+                self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor) + __CCENTERSIZE__ * __CLUSTERRESOLUTION__),pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__  + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor) + __CLUSTERRESOLUTION__ * __CCENTERSIZE__)))
+                self.controlcenter.constrainmod(uConstrain(pointA=uPoint(x = new_constraint.pointA.x + __HEADERSIZE__ * __CLUSTERRESOLUTION__ - __CLUSTERRESOLUTION__, y = new_constraint.pointB.y), pointB=uPoint(x = new_constraint.pointB.x, y = new_constraint.pointB.y + __CCENTERSIZE__ * __CLUSTERRESOLUTION__)))
+            elif self.header == "r":
+                #Set Widget Constraint
+                new_constraint = uConstrain(pointA = self.anchor, pointB = uPoint(x = self.anchor.x + self.width - (__HEADERSIZE__ * __CLUSTERRESOLUTION__), y = self.anchor.y + self.height - __CLUSTERRESOLUTION__ * __CCENTERSIZE__))
+                #Set CCenter Constraint
+                button_shrinking_factor = 0.3
+                self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor)),pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__  + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor) - __HEADERSIZE__ * __CLUSTERRESOLUTION__, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor))))
+                self.controlcenter.constrainmod(uConstrain(pointA=uPoint(x = new_constraint.pointA.x, y = new_constraint.pointB.y - __CCENTERSIZE__ * __CLUSTERRESOLUTION__), pointB=uPoint(x = new_constraint.pointB.x, y = new_constraint.pointB.y)))
+            elif self.header == "b":
+                #Set Widget Constraint
+                new_constraint = uConstrain(pointA = self.anchor, pointB = uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height - (__HEADERSIZE__ * __CLUSTERRESOLUTION__) - __CLUSTERRESOLUTION__ * __CCENTERSIZE__))
+                #Set CCenter Constraint
+                button_shrinking_factor = 0.3
+                self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor) + __CCENTERSIZE__ * __CLUSTERRESOLUTION__ * 1.5),pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__  + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor) - __HEADERSIZE__ * __CLUSTERRESOLUTION__+ __CCENTERSIZE__ * __CLUSTERRESOLUTION__ * 1.5)))
+                self.controlcenter.constrainmod(uConstrain(pointA=uPoint(x = new_constraint.pointA.x, y = new_constraint.pointB.y), pointB=uPoint(x = new_constraint.pointB.x, y = new_constraint.pointB.y + __CCENTERSIZE__ * __CLUSTERRESOLUTION__)))
+            else:
+                #Set CCenter Constraint
+                button_shrinking_factor = 0.3
+                self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor)),pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__  + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor))))
+                self.controlcenter.constrainmod(uConstrain(pointA=uPoint(x = new_constraint.pointA.x, y = new_constraint.pointB.y - __CCENTERSIZE__ * __CLUSTERRESOLUTION__), pointB=uPoint(x = new_constraint.pointB.x, y = new_constraint.pointB.y)))
+                #Set Widget Constraint
+                return self.child.constrainmod(uConstrain(pointA = self.anchor, pointB=uPoint(x = self.anchor.x + self.width, y = self.anchor.y + self.height - __CLUSTERRESOLUTION__ * __CCENTERSIZE__)))
+
+
         self.childs_constraint = new_constraint.copy
         self.child.constrainmod(new_constraint.copy)
-        __CCENTERSIZE__ = NotifyService.get("debug.widget-controlcenter-thickness-in-clusters")
+ 
         if self.ccenterSettingsPanel != None:
             settings_constraint = new_constraint.copy
             settings_constraint.pointA.x += __CLUSTERRESOLUTION__
             settings_constraint.pointA.y += __CLUSTERRESOLUTION__
             settings_constraint.pointB.x -= __CLUSTERRESOLUTION__
-            settings_constraint.pointB.y -= __CLUSTERRESOLUTION__ * __CCENTERSIZE__
             self.ccenterSettingsPanel.constrainmod(settings_constraint)
-        if self.controlcenter.status != "Closed":
-            button_shrinking_factor = 0.3
-            self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ - 1) / (2) - (__CLUSTERRESOLUTION__ * button_shrinking_factor)),pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__  + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor), new_constraint.pointB.y - __CLUSTERRESOLUTION__ * (__CCENTERSIZE__ + 1) / (2) + (__CLUSTERRESOLUTION__ * button_shrinking_factor))))
-            self.controlcenter.constrainmod(uConstrain(pointA=uPoint(x = new_constraint.pointA.x, y = new_constraint.pointB.y - __CCENTERSIZE__ * __CLUSTERRESOLUTION__), pointB=uPoint(x = new_constraint.pointB.x, y = new_constraint.pointB.y)))
-        else:
-            self.controlcenter.constrainmod(uConstrain(pointA=uPoint(0,0), pointB = uPoint(0,0)))
-            self.controlcenterOpenButton.constrainmod(uConstrain(pointA = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * 0.6, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * 0.6),pointB = uPoint(new_constraint.pointB.x - __CLUSTERRESOLUTION__ * 0.2, new_constraint.pointB.y - __CLUSTERRESOLUTION__ * 0.2)))
-
+            
 
     @tlog
     def miscmod(self):
